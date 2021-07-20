64de16e6c3cb16079c77c0130a36554c107ad134
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-07-20 20:00:13 +0530

    enable h2-console web access in spring security

--- user-ws/src/main/java/com/amsidh/mvc/usermicroservice/config/SecurityConfig.java
+++ user-ws/src/main/java/com/amsidh/mvc/usermicroservice/config/SecurityConfig.java
@@ -13,5 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/**").permitAll();
+        //Allow H2 Console web access
+        http.headers().frameOptions().disable();
     }
 }
