5389c23e4be562f3b32d298fa1c6e1486ca7fcd4
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-07-21 13:50:46 +0530

    Check jwt token validation using secret key

--- gateway-ws/src/main/java/com/amsidh/mvc/apigateway/filters/JwtTokenValidationFilter.java
+++ gateway-ws/src/main/java/com/amsidh/mvc/apigateway/filters/JwtTokenValidationFilter.java
@@ -1,14 +1,18 @@
 package com.amsidh.mvc.apigateway.filters;
 
+import io.jsonwebtoken.Jwts;
 import org.apache.http.HttpHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Component
 public class JwtTokenValidationFilter extends AbstractGatewayFilterFactory<JwtTokenValidationFilter.Config> {
@@ -23,11 +27,29 @@ public class JwtTokenValidationFilter extends AbstractGatewayFilterFactory<JwtTo
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+            ServerHttpRequest httpRequest = exchange.getRequest();
+            if (!httpRequest.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                 return onError(exchange, "Request does not contain Authorization header", HttpStatus.UNAUTHORIZED);
             }
+            String authorizationHeader = httpRequest.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwtToken = authorizationHeader.replace("Bearer", "");
+            if(!validateJwtToken(jwtToken)){
+                return onError(exchange, "Jwt token is invalid", HttpStatus.UNAUTHORIZED);
+            }
+
             return chain.filter(exchange);
         };
+    }
+
+    private boolean validateJwtToken(String jwtToken) {
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("jwt.secret.salt"))
+                .parseClaimsJws(jwtToken)
+                .getBody().getSubject();
+        if(Optional.ofNullable(userId).isPresent()){
+            return true;
+        }
+        return false;
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String errorMessage, HttpStatus httpStatus) {
