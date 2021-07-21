79129c9bd15b22391d6db76b0d2d16e7e1e35d9e
Amsidh Lokhande <amsidhlokhande@gmail.com>
2021-07-21 13:16:45 +0530

    Checking the Authorization header present in the request when JwtTokenValidationFilter is configured in route

---
+++ gateway-ws/src/main/java/com/amsidh/mvc/apigateway/filters/JwtTokenValidationFilter.java
@@ -0,0 +1,42 @@
+package com.amsidh.mvc.apigateway.filters;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtTokenValidationFilter extends AbstractGatewayFilterFactory<JwtTokenValidationFilter.Config> {
+
+    private final Environment environment;
+
+    public JwtTokenValidationFilter(Environment environment) {
+        super(Config.class);
+        this.environment = environment;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "Request does not contain Authorization header", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMessage, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    public static class Config {
+    }
+
+}
